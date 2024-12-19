8bbd96819b0ec15d1bdb4748dcb7b09eae5d34ea
degrandelucas <lucasdegrande15@gmail.com>
2024-12-19 16:51:44 -0300

    adicionando classe Conversion

--- src/cep/application/App.java
+++ src/cep/application/App.java
@@ -1,6 +1,8 @@
 package cep.application;
 
 import cep.connection.Connection;
+import cep.connection.Conversion;
+import cep.dto.InfoCEP;
 
 import java.util.Scanner;
 
@@ -10,10 +12,19 @@ public class App {
         System.out.println("Digite o CEP da cidade: ");
         String cep = readCEP.nextLine();
 
+        String responseBodyAPI = Connection.requestCEP(cep);
+
+        //Retorno da conversao de Json padrao da API para String da classe record InfoCEP (com os atributos desejados)
+        InfoCEP infoCEPFromJson = Conversion.conversionFromJson(responseBodyAPI);
+        System.out.println(infoCEPFromJson);
+
+        //Retorno da conversao da String da classe record InfoCEP para Json formatado na saida
+        String infoCEPToJson = Conversion.conversionToJson(infoCEPFromJson);
+        System.out.println(infoCEPFromJson);
+
         //print da API completa do CEP digitado
-        System.out.println(Connection.requestCEP(cep));
-
-
+        //System.out.println(Connection.requestCEP(cep));
+        
         readCEP.close();
 
     }
--- src/cep/connection/Conversion.java
+++ src/cep/connection/Conversion.java
@@ -1,18 +1,18 @@
 package cep.connection;
 
+import cep.dto.InfoCEP;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class Conversion {
 
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
-/*
-    public static String conversionFromJson{
-        return;
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public static InfoCEP conversionFromJson(String responseBodyAPI) {
+        return gson.fromJson(responseBodyAPI, InfoCEP.class);
     }
 
-    public static String conversionToJson{
-        return;
+    public static String conversionToJson (InfoCEP infoCEP){
+        return gson.toJson(InfoCEP.class);
     }
-*/
 }
